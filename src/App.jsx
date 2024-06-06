883714891973a5203b6f1513ba6b2f880a654693
Yanis Baroudi <yanis.baroudi@etu.umontpellier.fr>
2024-06-06 17:27:51 +0200

    format mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-  
-
   return (
-    <div>
-      <div>
-        <Outlet></Outlet>
+    <div className="app-container">
+      <div className="content">
+        <Outlet />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
